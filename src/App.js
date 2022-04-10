6b5dce4308abeeabae77cf78da48392a167adaa0
Ritesh1511 <riteshpc12@gmail.com>
2022-04-11 01:52:29 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import Pet from "./Pet";
+const App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div>
+     <h1>Adopt Me!</h1>  
+     <Pet name="Luna" animal="Dog" breed="Havanese" />
+     <Pet name="Pepper" animal="Bird" breed="Cockateil" />
+     <Pet name="Beam" animal="Dog" breed="Wheaten Terrier" />
+      </div>
   );
+
 }
 
 export default App;
---
+++ src/Pet.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Pet = (props) =>{
+    return(
+        <div id="blah">
+            <h2>{props.name}</h2>
+            <h2>{props.animal}</h2>
+            <h2>{props.breed}</h2>
+        </div>
+    )
+}
+export default Pet;
