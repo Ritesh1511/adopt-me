4d68ad8ac441ff9ca6d00a01525e34fc2b717524
Ritesh1511 <riteshpc12@gmail.com>
2022-04-27 15:34:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import Pet from "./Pet";
-const App=()=> {
-  return (
-     <div>
-     <h1>Adopt Me!</h1>  
-     <Pet name="Luna" animal="Dog" breed="Havanese" />
-     <Pet name="Pepper" animal="Bird" breed="Cockateil" />
-     <Pet name="Beam" animal="Dog" breed="Wheaten Terrier" />
-      </div>
-  );
+//import { render } from "react-dom";
+import SearchParams from "./SearchParams";
+import { StrictMode, useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
-}
+const App = () => {
+  const theme = useState("darkblue");
+  return (
+    <StrictMode>
+      <ThemeContext.Provider value={theme}>
+        <BrowserRouter>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeContext.Provider>
+    </StrictMode>
+  );
+};
 
 export default App;
